d8c924e5fc24edfc364a37bd8a73a60d201ccfd3
Zach Cronin <zcronin1@gmail.com>
2023-05-02 22:16:02 -0400

    Created button and canvas variables, adds clear button

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@
 // Ideas
 
 // animated cursors
+
+// Tile size buttons
+const largeTilesBtn = document.getElementById("large-tiles");
+const mediumTilesBtn = document.getElementById("medium-tiles");
+const smallTilesBtn = document.getElementById("small-tiles");
+
+// Color buttons
+const rainbowColorsBtn = document.getElementById("unicorn-vomit");
+const hogwartsColorsBtn = document.getElementById("in-the-house");
+const matrixColorsBtn = document.getElementById("enter-the-matrix");
+
+// Clear canvas button
+const clearBtn = document.getElementById("clear-canvas")
+
+// Pixel canvas
+const canvas = document.getElementById("inner-canvas");
