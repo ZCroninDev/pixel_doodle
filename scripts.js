0cc210d8c8c48fa5517aa8e594dbe7ec9435fbb6
Zach Cronin <zcronin1@gmail.com>
2023-05-16 20:14:06 -0400

    Adds color, grid size, and default variables.

--- scripts.js
+++ scripts.js
@@ -8,13 +8,35 @@ const largeTilesBtn = document.getElementById("large-tiles");
 const mediumTilesBtn = document.getElementById("medium-tiles");
 const smallTilesBtn = document.getElementById("small-tiles");
 
+// Tile sizes
+const largeTiles = 16;
+const mediumTiles = 32;
+const smallTiles = 64;
+
 // Color buttons
 const rainbowColorsBtn = document.getElementById("unicorn-vomit");
 const hogwartsColorsBtn = document.getElementById("in-the-house");
 const matrixColorsBtn = document.getElementById("enter-the-matrix");
 
+// Colors
+
+const rainbowColors = ['#F60000',  '#FF8C00', '#FFEE00', '#4DE94C', '#3783FF', '#4815AA']
+const hogwartsColors = ['#9c1203', '#e3a000', '#033807', '#00165e']
+const matrixColors = ['#0D0208', '#003B00', '#008F11', '#00FF41']
+
+
 // Clear canvas button
 const clearBtn = document.getElementById("clear-canvas")
 
 // Pixel canvas
-const canvas = document.getElementById("inner-canvas");
+const canvas = document.getElementById("grid");
+
+// Defaults
+const defaultColor = rainbowColorsBtn;
+const defaultSize = 32;
+
+document.onload.getdefaults()
+
+function getDefaults {
+    
+}
